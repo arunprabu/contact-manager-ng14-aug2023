1ace7672c1e3ad5e23a3aeba67ea259ea55a55bf
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-09-08 16:20:37 +0530

    updated

--- src/app/contact-manager/components/add-contact/add-contact.component.ts
+++ src/app/contact-manager/components/add-contact/add-contact.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact',
   templateUrl: './add-contact.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class AddContactComponent implements OnInit {
+  // Step 1: Have the ts equivalent for the html form tag using FormGroup
+  addContactForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    // Step 1 continues ....
+    this.addContactForm = new FormGroup({
+      // Step 2: Have the ts equivalent for the html form elements
+      name: new FormControl('', Validators.required), // Step 5: setup validator
+      phone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]) 
+    });
   }
 
+  ngOnInit(): void {}
+
+  addContact() {
+    console.log(this.addContactForm);
+  }
 }
--- src/app/contact-manager/contact-manager.module.ts
+++ src/app/contact-manager/contact-manager.module.ts
@@ -4,6 +4,7 @@ import { ContactManagerRoutingModule } from './contact-manager-routing.module';
 import { ContactListComponent } from './components/contact-list/contact-list.component';
 import { AddContactComponent } from './components/add-contact/add-contact.component';
 import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,6 @@ import { ContactDetailsComponent } from './components/contact-details/contact-de
     AddContactComponent,
     ContactDetailsComponent
   ],
-  imports: [CommonModule, ContactManagerRoutingModule]
+  imports: [CommonModule, ContactManagerRoutingModule, ReactiveFormsModule]
 })
 export class ContactManagerModule {}
